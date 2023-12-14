512aaab26542c2edd6b620463f3e754444905533
Haris Kordić <kordicharis18@gmail.com>
2023-12-14 14:35:57 +0100

    add: data model

---
+++ data_models/models.py
@@ -0,0 +1,11 @@
+class Country:
+    id: int
+    name: string
+    cpi_index: float
+    seed_funding_index: float
+
+    def __init__(self):
+        self.id = 0
+        self.name = ''
+        self.cpi_index = 0
+        self.seed_funding_index = 0
